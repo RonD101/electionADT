d7c87d9ed2dd66658923252239aba0e6d1d07d7a
rond <ron.dahan@campus.technion.ac.il>
2020-04-14 18:58:25 +0300

    Stupid ADT

---
+++ cmake-build-debug/CMakeFiles/electionADT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/electionADT.dir/main.c.o"
+  "electionADT"
+  "electionADT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/electionADT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/electionADT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for electionADT.
+# This may be replaced when dependencies are built.
