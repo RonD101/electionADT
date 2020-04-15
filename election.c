104bc757bf8db8b6cf6045d2682f87dd7ef08d72
yoav <kenkendler123@gmail.com>
2020-04-15 11:34:47 +0300

    fuck lib

--- election.c
+++ election.c
@@ -4,6 +4,8 @@
 
 #include "mtm_map/map.h"
 #include "election.h"
+#include <string.h>
+#include <math.h>
 
 static char* toString(int num);
 static int toInt(char* str);
@@ -19,6 +21,14 @@ static char* toString(int num){
 
 }
 
-static int toInt(char* str){
-
+static int toInt(char* str)
+{
+    int i = strlen(str);
+    int num = 0;
+    while(i > 0)
+    {
+        num += str[i - 1] * pow(10, strlen(str) - i);
+        i--;
+    }
+    return num;
 }
